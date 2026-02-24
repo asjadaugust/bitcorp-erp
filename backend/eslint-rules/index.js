739f3a0981ab7627b5c9c7eba3e3b82a84b33d75
Mohammad Asjad <asjad.august@gmail.com>
2026-02-24 22:16:42 +0100

    chore(eslint): scaffold eslint-plugin-bitcorp directory structure

---
+++ backend/eslint-rules/index.js
@@ -0,0 +1,8 @@
+'use strict';
+
+module.exports = {
+  rules: {
+    'dto-spanish-snake-case': require('./rules/dto-spanish-snake-case'),
+    'entity-spanish-camel-case': require('./rules/entity-spanish-camel-case'),
+  },
+};
